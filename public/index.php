44858fbe148cc28c774668093a2c844aad730dad
Roël Couwenberg <rcacouwenberg@gmail.com>
2022-02-04 13:02:15 +0100

    Init

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$page['Title'] = "Bureau Onbeperkte Zaken";
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link async="" rel="preload" href="https://avametix.com/stylesheets/stylesheet.css?ver=0" as="style">
+        <link async="" href="https://avametix.com/stylesheets/stylesheet.css?ver=0" rel="stylesheet" type="text/css">
+        <title><?=$page['Title']?> - Avametix</title>
+    </head>
+    <body>
+        <header>
+            <a href="https://avametix.com" class="image"><img src="https://avametix.com/media/assets/icons/Logo-White.svg" alt="Avametix logo" width="40"></a>
+        </header>
+        <main>
+            <div class="center off">
+                <h1>Bureau Onbeperkte Zaken</h1>
+                <p>
+                    SO-L, 04/02/2022, Laravel (PHP 8.1)
+                </p>
+            </div>
+        </main>
+        <script src="https://avametix.com/javascript/javascript.js"></script>
+    </body>
+</html>
