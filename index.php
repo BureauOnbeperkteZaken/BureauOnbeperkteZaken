644802f412a5f44abbcf5eb2394a7a9f52bff8c3
Roël Couwenberg <rcacouwenberg@gmail.com>
2022-02-04 12:24:14 +0100

    Created base index

--- index.php
+++ index.php
@@ -1,60 +1,29 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+$page['Title'] = "Bureau Onbeperkte Zaken";
 
-define('LARAVEL_START', microtime(true));
+?>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link async="" rel="preload" href="https://avametix.com/stylesheets/stylesheet.css?ver=0" as="style">
+        <link async="" href="https://avametix.com/stylesheets/stylesheet.css?ver=0" rel="stylesheet" type="text/css">
+        <title><?=$page['Title']?> - Avametix</title>
+    </head>
+    <body>
+        <header>
+            <a href="https://avametix.com" class="image"><img src="https://avametix.com/media/assets/icons/Logo-White.svg" alt="Avametix logo" width="40"></a>
+        </header>
+        <main>
+            <div class="center off">
+                <h1>Bureau Onbeperkte Zaken</h1>
+                <p>
+                    SO-L, 04/02/2022, Laravel (PHP 8.1)
+                </p>
+            </div>
+        </main>
+        <script src="https://avametix.com/javascript/javascript.js"></script>
+    </body>
+</html>
