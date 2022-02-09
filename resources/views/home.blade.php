7970599cf66c06613a47ad6548e8fc4d43c3a604
Roël Couwenberg <rcacouwenberg@gmail.com>
2022-02-09 13:13:28 +0100

    Changed project to reflect current environment.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,14 @@
-<?php
-
-$page['Title'] = "Bureau Onbeperkte Zaken";
-
+<?PHP
+    $docs_version = explode('.', Illuminate\Foundation\Application::VERSION)[0] . ".x";
 ?>
 
-<!DOCTYPE html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html lang="{{ config('app.locale') }}">
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link async="" rel="preload" href="https://avametix.com/stylesheets/stylesheet.css?ver=0" as="style">
         <link async="" href="https://avametix.com/stylesheets/stylesheet.css?ver=0" rel="stylesheet" type="text/css">
-        <title><?=$page['Title']?> - Avametix</title>
+        <title>{{ config('app.name') }} - Avametix</title>
     </head>
     <body>
         <header>
@@ -18,9 +16,9 @@ $page['Title'] = "Bureau Onbeperkte Zaken";
         </header>
         <main>
             <div class="center off">
-                <h1><?=$page['Title']?></h1>
+                <h1>{{ config('app.name') }}</h1>
                 <p>
-                    SO-L, 04/02/2022, <a href="https://laravel.com/docs/9.x">Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</a>, <a href="https://github.com/Pixel-Null/BureauOnbeperkteZaken">Github Repository</a><br/>
+                    SO-L, 04/02/2022, <a href="https://laravel.com/docs/<?=$docs_version?>">Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</a>, <a href="https://github.com/Pixel-Null/BureauOnbeperkteZaken">Github Repository</a><br/>
                     Hosted by <a href="https://avametix.com">Avametix</a>.
                 </p>
             </div>
