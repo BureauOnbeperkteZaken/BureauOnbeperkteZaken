d5fe013717254ae8746241f53ffcb31674eff7c9
PixelNull <rcacouwenberg@gmail.com>
2022-02-25 15:11:09 +0100

    Fixed failing test

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('app/home');
+});
+
 Route::get('/{page}', function ($page) {
     return view('app/' . $page);
 });
