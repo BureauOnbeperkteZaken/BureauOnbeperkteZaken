1a484d4db0dc1a104a9a3f723e289b309d5766cc
RayNelemans <r.nelemans1@student.avans.nl>
2022-03-14 17:36:48 +0100

    Opzetje video zichtbaar

---
+++ resources/views/onbeperkt-anders.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<body>
+    <div>
+        <?php
+        $videoLink = "https://player.vimeo.com/video/682661192";
+        ?>
+        <h1>Onbeperkt Anders</h1>
+
+        <iframe src=<?php echo $videoLink; ?> width="320" height="240" allowfullscreen></iframe>
+
+    </div>
+
+</body>
+</head>
+
+</html>
