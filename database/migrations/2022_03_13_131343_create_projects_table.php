95ff1332cb27e5637e89a1b21136d6537a75fd8b
RayNelemans <r.nelemans1@student.avans.nl>
2022-03-14 19:12:11 +0100

    Video ophalen uit database

--- database/migrations/2022_03_13_131343_create_projects_table.php
+++ database/migrations/2022_03_13_131343_create_projects_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('video-link');
+            $table->string('video_link');
         });
     }
 
