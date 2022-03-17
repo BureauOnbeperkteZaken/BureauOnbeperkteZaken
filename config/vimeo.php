940ab24f4198a79e6ee980184973c0a75dbfe782
Svenvdzwet <s.vanderzwet@student.avans.nl>
2022-03-17 22:20:51 +0100

    added the vimeo config

---
+++ config/vimeo.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ *   Copyright 2018 Vimeo.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'main',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Vimeo Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like.
+    |
+    */
+
+    'connections' => [
+
+        'main' => [
+            'client_id' => env('VIMEO_CLIENT', '89078c332bb34c43037f644899c8c0308ef86e57'),
+            'client_secret' => env('VIMEO_SECRET', '22ZYgVcBW/R8DaMOhGjWF3HBKKhZO9qk0R7xkqNm3AR6qY9CY2Cit08enCszBEjZY2rTt4z532RSaqZVYsukGNLruuR/TTdVGZHuckBOr39qd5IRqFFuBnvWpXD6M6xl'),
+            'access_token' => env('VIMEO_ACCESS', 'a1757b692c3b51481357dbc3aef53f92'),
+        ],
+
+        'alternative' => [
+            'client_id' => env('VIMEO_ALT_CLIENT', 'your-alt-client-id'),
+            'client_secret' => env('VIMEO_ALT_SECRET', 'your-alt-client-secret'),
+            'access_token' => env('VIMEO_ALT_ACCESS', null),
+        ],
+
+    ],
+
+];
