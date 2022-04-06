9bcd1c446d5fda39e9db2c2d53c95da5aa775138
Svenvdzwet <s.vanderzwet@student.avans.nl>
2022-04-06 21:49:09 +0200

    Added the components

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,45 +1,24 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<x-page.fullwidth :cards="true">
+    <div class="paragraph">
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('paneladd_user') }}">
+        <form method="post" action="{{route('paneladd_user')}}">
+
             @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Naam')" />
-
-                <x-input placeholder="Vul hier de naam in." dusk="name" id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input placeholder="Vul hier de email in." dusk="email" id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Wachtwoord')" />
-
-                <x-input placeholder="Vul hier je wachtwoord in." dusk="password" id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-            <div class="flex items-center justify-end mt-4">
-                <x-button dusk='register' class="ml-4">
-                    {{ __('Registreer') }}
-                </x-button>
-            </div>
+            <label for="name"><h4>Naam</h4></label>
+            <input placeholder="Vul hier de naam in." dusk="name" id="name" class="block mt-1 w-full" type="text" name="name" required value="{{old('name')}}" />
+            <label for="email"><h4>Email</h4></label>
+            <input placeholder="Vul hier de email in." dusk="email" id="email" class="block mt-1 w-full" type="email" name="email" value="{{old('email')}}" required />
+            <label for="password"><h4>Wachtwoord</h4></label>
+            <input placeholder="Vul hier het wachtwoord in." dusk="password" id="password" class="block mt-1 w-full" type="password" name="password" value="{{old('password')}}" required />
+            <br/>
+            <button dusk='register' class="ml-4">
+                {{ __('Log in') }}
+            </button>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+</x-page.fullwidth>
