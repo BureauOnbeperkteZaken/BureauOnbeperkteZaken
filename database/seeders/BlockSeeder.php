955168be81dabdf8320e9177d96f075e15690a04
DevonAvans <d.thomassen1@student.avans.nl>
2022-05-03 14:02:30 +0200

    Seeders and models

---
+++ database/seeders/BlockSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BlockSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('blocks')->insert([
+            'template_id' => 1,
+            'order' => 1,
+            'content' => '<h5>Subheading</h5><h1>Title</h1><p>Deserunt labore do mollit consectetur mollit sit ex dolor anim exercitation fugiat dolore fugiat Lorem. Et officia adipisicing Lorem aute ex in occaecat anim laborum cupidatat quis ut magna reprehenderit. Consectetur aute irure in dolor. Pariatur ea occaecat nisi officia esse non pariatur ullamco veniam est.</p>',
+        ]);
+        DB::table('blocks')->insert([
+            'template_id' => 1,
+            'order' => 2,
+            'content' => '<p>Tempor aliquip sint consequat Lorem proident sint nostrud aute ex adipisicing eu. Consequat exercitation et sint aliqua ullamco adipisicing ipsum ex. Occaecat eu do ipsum sunt velit culpa. Laborum aliqua exercitation consequat aute eiusmod ipsum sint irure sint reprehenderit ullamco.</p><ul><li>This is</li><li>An unordered</li><li>List</li></ul><ol><li>This is</li><li>An unordered</li><li>List</li></ol>',
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         ]);
         $this->call([LanguageSeeder::class]);
         $this->call([TemplateSeeder::class]);
+        $this->call([BlockSeeder::class]);
     }
 }
