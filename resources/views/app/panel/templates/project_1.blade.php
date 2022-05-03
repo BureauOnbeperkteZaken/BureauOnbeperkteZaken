cf0e9cf9947507018fdb4f24037453fb6b578999
DevonAvans <d.thomassen1@student.avans.nl>
2022-05-03 14:04:31 +0200

    Some blade project 1 testing code

--- resources/views/app/panel/templates/project_1.blade.php
+++ resources/views/app/panel/templates/project_1.blade.php
@@ -1,36 +1,13 @@
 <x-page.fullwidth :cards="true">
     <div class="editor">
         <div class="content">
-            <div class="paragraph">
-                <h5>Subheading</h5>
-                <h1>Title</h1>
-                <p>
-                    Deserunt labore do mollit consectetur mollit sit ex dolor anim exercitation fugiat dolore fugiat Lorem. Et officia adipisicing Lorem aute ex in occaecat anim laborum cupidatat quis ut magna reprehenderit. Consectetur aute irure in dolor. Pariatur ea occaecat nisi officia esse non pariatur ullamco veniam est.
-                </p>
-            </div>
-            <div class="paragraph">
-                <p>
-                    Tempor aliquip sint consequat Lorem proident sint nostrud aute ex adipisicing eu. Consequat exercitation et sint aliqua ullamco adipisicing ipsum ex. Occaecat eu do ipsum sunt velit culpa. Laborum aliqua exercitation consequat aute eiusmod ipsum sint irure sint reprehenderit ullamco.
-                </p>
-                <ul>
-                    <li>This is</li>
-                    <li>An unordered</li>
-                    <li>List</li>
-                </ul>
-                <ol>
-                    <li>This is</li>
-                    <li>An unordered</li>
-                    <li>List</li>
-                </ol>
-            </div>
-            <div class="paragraph">
-                <p>
-                    Non ad excepteur nisi amet pariatur elit laboris. Duis ullamco ut voluptate laboris. Officia veniam sit proident amet nostrud do. Eiusmod tempor eiusmod proident aute dolor laborum.
-                </p>
-            </div>
-        </div>
-        <div class="editor-toolbar">
-            <button><a href="{{route('template.edit', 1)}}">{{ __('edit') }}</a></button>
+            @foreach ($blocks as $block)
+            {!! $block->content !!}
+            <script>
+                let paragraph{{$block->order}} = document.querySelectorAll('.paragraph')[{{$block->order - 1}}];
+                paragraph{{$block->order}}.innerHTML += "<div class=\"editor-toolbar\"><button><a href=\"{{route('block.edit', $block->order)}}\">{{ __('edit') }}</a></button></div>"
+            </script>
+            @endforeach
         </div>
     </div>
     <hr data-title="{{ __('gallery') }}" />
@@ -64,4 +41,4 @@
             </div>
         </div>
     </div>
-</x-page.sidebar>
+    </x-page.sidebar>
