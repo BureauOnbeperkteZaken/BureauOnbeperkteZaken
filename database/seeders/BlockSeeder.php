58fc5e44c2ed67b97b6e8891bacc2ec18a2c4f74
RayNelemans <r.nelemans1@student.avans.nl>
2022-05-03 16:31:43 +0200

    Small fix in Block seeder
    Fixed error with seeder

--- database/seeders/BlockSeeder.php
+++ database/seeders/BlockSeeder.php
@@ -18,12 +18,12 @@ class BlockSeeder extends Seeder
         DB::table('blocks')->insert([
             'template_id' => 1,
             'order' => 1,
-            'content' => '<h5>Subheading</h5><h1>Title</h1><p>Deserunt labore do mollit consectetur mollit sit ex dolor anim exercitation fugiat dolore fugiat Lorem. Et officia adipisicing Lorem aute ex in occaecat anim laborum cupidatat quis ut magna reprehenderit. Consectetur aute irure in dolor. Pariatur ea occaecat nisi officia esse non pariatur ullamco veniam est.</p>',
+            'content' => '<div class="paragraph"><h5>Subheading</h5><h1>Title</h1><p>Deserunt labore do mollit consectetur mollit sit ex dolor anim exercitation fugiat dolore fugiat Lorem. Et officia adipisicing Lorem aute ex in occaecat anim laborum cupidatat quis ut magna reprehenderit. Consectetur aute irure in dolor. Pariatur ea occaecat nisi officia esse non pariatur ullamco veniam est.</p></div>',
         ]);
         DB::table('blocks')->insert([
             'template_id' => 1,
             'order' => 2,
-            'content' => '<p>Tempor aliquip sint consequat Lorem proident sint nostrud aute ex adipisicing eu. Consequat exercitation et sint aliqua ullamco adipisicing ipsum ex. Occaecat eu do ipsum sunt velit culpa. Laborum aliqua exercitation consequat aute eiusmod ipsum sint irure sint reprehenderit ullamco.</p><ul><li>This is</li><li>An unordered</li><li>List</li></ul><ol><li>This is</li><li>An unordered</li><li>List</li></ol>',
+            'content' => '<div class="paragraph"><p>Tempor aliquip sint consequat Lorem proident sint nostrud aute ex adipisicing eu. Consequat exercitation et sint aliqua ullamco adipisicing ipsum ex. Occaecat eu do ipsum sunt velit culpa. Laborum aliqua exercitation consequat aute eiusmod ipsum sint irure sint reprehenderit ullamco.</p><ul><li>This is</li><li>An unordered</li><li>List</li></ul><ol><li>This is</li><li>An unordered</li><li>List</li></ol></div>',
         ]);
     }
 }
