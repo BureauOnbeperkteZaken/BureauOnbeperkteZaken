0ba8b51cf54e402ef6076b0bbd6177b7066f04b6
DevonAvans <d.thomassen1@student.avans.nl>
2022-05-11 15:34:11 +0200

    Remove unused functionality from Template

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Block;
-use App\Models\Language;
 use Illuminate\Http\Request;
 use App\Models\TemplateBlock;
 
@@ -13,35 +11,5 @@ class TemplateController extends Controller
     {
         $blocks = TemplateBlock::where('template_id', $id)->orderBy('order', 'asc')->get();
         return view('app.panel.templates.project_' . $id, compact('blocks'));
-    }
-
-    // Legacy code don't delete yet
-    public function edit($id = 0)
-    {
-        $template = TemplateBlock::find($id);
-        $languages = Language::all();
-
-        return view('app.panel.editor', compact('template', 'languages'));
-    }
-
-    // Legacy code don't delete yet
-    public function update(Request $request, TemplateBlock $template)
-    {
-        $template->content = $request->content;
-        $template->language_code = $request->language_code;
-
-        $template->save();
-
-        return view('app.panel.templates.project_' . $template->id);
-    }
-
-    public function store(Request $request)
-    {
-        $template = new TemplateBlock();
-        $template->id = 0;
-        $template->language_code = $request->language_code;
-        $template->content = $request->content;
-
-        $template->save();
     }
 }
