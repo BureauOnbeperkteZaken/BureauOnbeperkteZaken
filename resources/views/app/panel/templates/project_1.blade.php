95e9aacf2b044e8d4c88f1e972c4834309372e2a
DevonAvans <d.thomassen1@student.avans.nl>
2022-05-11 13:17:50 +0200

    Removal of legancy code

--- resources/views/app/panel/templates/project_1.blade.php
+++ resources/views/app/panel/templates/project_1.blade.php
@@ -21,35 +21,4 @@
             <button><a href="{{route('block.create', ['template' => $template->id, 'type' => 'gallery'])}}">Gallery</a></button>
         </div>
     </div>
-    <!-- <hr data-title="{{ __('gallery') }}" /> -->
-    <!-- <div class="gallery">
-        <div class="photo">
-            <img src="https://www.kidsproof.nl/images/Image350_235/browniesendownies_browniesendownies_3_350_235.jpg" />
-            <div class="details">
-                <span property="name">Brownies en downies</span>
-                <span property="date">Datum</span>
-            </div>
-        </div>
-        <div class="photo">
-            <img src="https://media.prdn.nl/retailtrends/images/brownies%26downies%282%29.jpg?w=850" />
-            <div class="details">
-                <span property="name">Foto van iets</span>
-                <span property="date">Datum</span>
-            </div>
-        </div>
-        <div class="photo">
-            <img src="https://sleutelstad.nl/wp-content/uploads/2020/05/022020-Summer-en-Erwin-2-1800x900.jpg" />
-            <div class="details">
-                <span property="name">Foto van iets</span>
-                <span property="date">Datum</span>
-            </div>
-        </div>
-        <div class="photo">
-            <img src="https://denationalefranchisegids.nl/wp-content/uploads/2020/04/Brownies_en_Downies_Franchise.jpg" />
-            <div class="details">
-                <span property="name">Foto van iets</span>
-                <span property="date">Datum</span>
-            </div>
-        </div>
-    </div> -->
 </x-page.sidebar>
