fd92107fe74f2d279689a6a5615f01d03f9a2aff
DevonAvans <d.thomassen1@student.avans.nl>
2022-05-11 15:55:04 +0200

    rename template builder

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -10,6 +10,6 @@ class TemplateController extends Controller
     public function read($id)
     {
         $blocks = TemplateBlock::where('template_id', $id)->orderBy('order', 'asc')->get();
-        return view('app.panel.templates.builder', compact('blocks'));
+        return view('app.panel.template-builder', compact('blocks'));
     }
 }
