3216a641376170b733b00f2822c67beec7a5ef71
DevonAvans <d.thomassen1@student.avans.nl>
2022-05-12 16:40:16 +0200

    template builder fix

--- resources/views/app/panel/template-builder.blade.php
+++ resources/views/app/panel/template-builder.blade.php
@@ -10,11 +10,5 @@
             {!! $block->content !!}
             @endforeach
         </div>
-        <div class="flex">
-            <button><a href="{{route('panelblock.create', ['template' => $block->template_id, 'type' => 'paragraph'])}}">Paragraph</a></button>
-            <button><a href="{{route('panelblock.create', ['template' => $block->template_id, 'type' => 'image-paragraph'])}}">Image-paragraph</a></button>
-            <button><a href="{{route('panelblock.create', ['template' => $block->template_id, 'type' => 'paragraph-image'])}}">Paragraph-image</a></button>
-            <button><a href="{{route('panelblock.create', ['template' => $block->template_id, 'type' => 'gallery'])}}">Gallery</a></button>
-        </div>
     </div>
 </x-page.sidebar>
