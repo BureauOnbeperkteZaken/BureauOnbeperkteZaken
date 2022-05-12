2325d93295e844ef4da57d1ae9a87054db145042
DevonAvans <d.thomassen1@student.avans.nl>
2022-05-12 19:41:59 +0200

    video in project builder

--- resources/views/app/project-viewer.blade.php
+++ resources/views/app/project-viewer.blade.php
@@ -6,7 +6,7 @@
 <x-page.fullwidth :cards="true">
 
     <x-slot:background>
-        <iframe id="amongus" type="text/html" title="Vimeo player" src="{{ $videoLink }}" allowfullscreen></iframe>
+        <iframe id="bigVideo" type="text/html" title="Vimeo player" src="{{ $videoLink }}" allowfullscreen></iframe>
     </x-slot:background>
 
     <div class="editor">
