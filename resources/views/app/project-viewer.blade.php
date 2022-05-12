1e6013e67c4ab45d71a2169b35a3c93192d3b3d3
DevonAvans <d.thomassen1@student.avans.nl>
2022-05-12 19:35:48 +0200

    Toolbar upgrade 😎

--- resources/views/app/project-viewer.blade.php
+++ resources/views/app/project-viewer.blade.php
@@ -6,8 +6,7 @@
 <x-page.fullwidth :cards="true">
 
     <x-slot:background>
-    <iframe id="amongus" type="text/html" title="Vimeo player"
-        src="{{ $videoLink }}" allowfullscreen></iframe>
+        <iframe id="amongus" type="text/html" title="Vimeo player" src="{{ $videoLink }}" allowfullscreen></iframe>
     </x-slot:background>
 
     <div class="editor">
