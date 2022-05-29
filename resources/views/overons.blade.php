426d33af96956aca8e4961a48f0561fb436876a8
kjellgeerlings <k.geerlings@student.avans.nl>
2022-05-29 21:28:34 +0200

    added pictures and text to make it like the real page

--- resources/views/overons.blade.php
+++ resources/views/overons.blade.php
@@ -5,15 +5,128 @@ $docs_version = explode('.', Illuminate\Foundation\Application::VERSION)[0] . ".
 <x-page.fullwidth>
 
     <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        </head>
-        <x-slot name="header">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <x-slot name="header">
         <h2>
-                {{ __('Dashboard') }}
-            </h2>
-        </x-slot>
-        
-        
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+
+    <section>
         <h2 class="text-left mt-5">Ideeënmakers</h2>
-        <label for="1" class="text-center" dusk="template"><img src="{{asset('img/Jojo_placeholder.jpg')}}"/></label>
+        <label for="1" class="text-center" dusk="template"><img src="{{asset('img/Jojo_placeholder.jpg')}}" /></label>
+        <h4>Jojo</h4>
+
+        <div class="paragraph" style="text-align: left;">
+            Wij zijn Carine Hermens (programmamaker/regisseur/docent) en Jojo Heesbeen (regisseur/, klinisch psycholoog
+            en filosoof) en vakspecialisten bij theaterwerkplaats Novalis.
+            <br><br>
+            Wij werken al jaren met mensen met een beperking en vinden het nodig om een revolutie teweeg te brengen
+            omdat mensen met een beperking nog altijd worden gediscrimineerd in onze samenleving.
+            Bijvoorbeeld vanuit de Wajong werken en geen passend salaris ontvangen voor de administratieve werkzaamheden
+            en zodoende
+            niet uit de Wajong kunnen komen en bang zijn om de baan kwijt te raken als hij dit aankaart.
+            <br>
+            Als het om theater maken gaat, er is geen castingbureau of afdeling voor mensen met een handicap.
+            Ze kunnen geen carrière opbouwen met de talenten die ze hebben, ze worden een keer gevraagd en daarna is het
+            klaar.
+            Een biertje drinken kan ook een probleem zijn als je een beperking hebt. Vaak wordt er naar de leeftijd
+            gevraagd of ze krijgen het niet.
+            Een huis met een tuintje is niet mogelijk voor iemand met een beperking. En ga zo maar door.
+            Na jaren van landelijk campagnes en lobby’s worden mensen met een beperking nog altijd genegeerd als het
+            gaat om de basisrechten van de mensheid;
+            vrijheid van mening en zelfbeschikkingsrecht.
+            Dit vinden wij niet alleen, maar ook de mensen met een beperking.
+            Het VN verdrag voor mensen met een beperking krijgt geen aandacht. Dat willen wij bekend maken.
+        </div>
+    </section>
+
+    <br><br>
+
+    <section>
+        <h2>Het bestuur</h2>
+        <style>
+            img {
+                width: 300px;
+                height: 300px;
+                object-fit: cover;
+            }
+
+            .row {
+                display: flex;
+                flex-wrap: wrap;
+            }
+
+            .column {
+                flex: 50%;
+            }
+
+            .item {
+                padding: 5%;
+            }
+        </style>
+        <div class="container">
+
+            <div class="row">
+                <div class="column">
+                    <div class="item">
+                        <div class="frame">
+                            <img src="{{asset('img/Jojo_placeholder.jpg')}}" />
+                        </div>
+                        <!-- TODO change the class to something more fitting -->
+                        <div class="">
+                            <h5>Jojo</h5>
+                            <div class="paragraph" style="text-align: left;">
+                                Wil de maatschappij laten zien, dat talenten van mensen met een handicap omarmend moet
+                                worden en niet ontkend.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="frame">
+                            <img src="{{asset('img/placeholder1.jpg')}}" />
+                        </div>
+                        <div class="">
+                            <h5>Lianne</h5>
+                            <div class="paragraph" style="text-align: left;">
+                                Voelt me uitgedaagd om een steentje bij te dragen aan een wereld waarin iedereen gehoord
+                                en gezien wordt, en serieus genomen in de mogelijkheden die er wél zijn.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="item">
+                        <div class="frame">
+                            <img src="{{asset('img/placeholder2.jpg')}}" />
+                        </div>
+                        <div class="">
+                            <h5>Hans</h5>
+                            <div class="paragraph" style="text-align: left;">
+                                Mijn grote wens is dat onze gehandicapte medemensen optimaal kunnen functioneren in onze
+                                samenleving op basis van gelijkwaardigheid en hun kwaliteiten.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="frame">
+                        <img src="{{asset('img/placeholder3.jpg')}}" />
+                    </div>
+                    <div class="">
+                        <h5>Carine</h5>
+                        <div class="paragraph" style="text-align: left;">
+                            Wil graag de rest van de wereld laten zien hoe kwaliteiten van mensen met een beperking veel
+                            beter ingezet kunnen worden in de maatschappij.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
 </x-page.fullwidth>
