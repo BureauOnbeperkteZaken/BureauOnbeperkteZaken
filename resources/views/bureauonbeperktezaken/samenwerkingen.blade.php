6c554f8dcdc7b37eb0d4c445b7dad1843bf63eb0
RayNelemans <r.nelemans1@student.avans.nl>
2022-05-30 17:33:48 +0200

    Button fix

--- resources/views/bureauonbeperktezaken/samenwerkingen.blade.php
+++ resources/views/bureauonbeperktezaken/samenwerkingen.blade.php
@@ -8,7 +8,7 @@
             <div class="text-test">
                 <h2>Gemeente Vught</h2>
                 <p>Netty Verhagen Anja de Jonge</p>
-                <a class="button" href="https://vught.nl/">
+                <a class="button" href="https://vught.nl/" target="blank">
                     Lees meer
                 </a>
             </div>
@@ -20,22 +20,22 @@
             <div class="text-test">
                 <h2>Moved Media</h2>
                 <p>Peter van den Dungen</p>
-                <a class="button" href="https://www.movedmedia.nl//">
+                <a class="button" href="https://www.movedmedia.nl/" target="blank">
                     Lees meer
                 </a>
             </div>
         </div>
     </div>
-    <h1>Comité van aanbevelingen</h1>
+    <h2>Comité van aanbevelingen</h2>
     <div class="paragraph flex-test">
         <div class="flex-test-inline">
             <div class="photo-test">
                 <img src="{{ asset('storage/uploads/ZomaarZichtbaar.jpg') }}" alt="Logo Zomaar Zichtbaar">
             </div>
             <div class="text-test">
-                <h2>Maarten Zomers</h2>
+                <h3>Maarten Zomers</h3>
                 <p>Maarten Zomers</p>
-                <a class="button" href="https://zomaarzichtbaar.nl/">
+                <a class="button" href="https://zomaarzichtbaar.nl/" target="blank">
                     Lees meer
                 </a>
             </div>
@@ -45,9 +45,9 @@
                 <img src="{{ asset('storage/uploads/Scarable.png') }}" alt="Logo Scarable">
             </div>
             <div class="text-test">
-                <h2>Scarabee Films</h2>
+                <h3>Scarabee Films</h3>
                 <p>Hetty Naaijkens</p>
-                <a class="button" href="https://scarabeefilms.com/">
+                <a class="button" href="https://scarabeefilms.com/" target="blank">
                     Lees meer
                 </a>
             </div>
@@ -57,9 +57,9 @@
                 <img src="{{ asset('storage/uploads/NoortjeVanLith.jpg') }}" alt="Foto Noortje van Lith">
             </div>
             <div class="text-test">
-                <h2>Noortje van Lith</h2>
+                <h3>Noortje van Lith</h3>
                 <p>Op wielen! Soms is er een aanpassing nodig voor gelijke behandeling</p>
-                <a class="button" href="https://www.noortjevanlith.nl/">
+                <a class="button" href="https://www.noortjevanlith.nl/" target="blank">
                     Lees meer
                 </a>
             </div>
@@ -69,9 +69,9 @@
                 <img src="{{ asset('storage/uploads/van-kruisdijk.jpg') }}" alt="Logo van Kruisdijk">
             </div>
             <div class="text-test">
-                <h2>Van Kruisdijk Notarissen</h2>
+                <h3>Van Kruisdijk Notarissen</h3>
                 <p>Notaris Kruijsdijk</p>
-                <a class="button" href="https://www.vankruijsdijknotarissen.nl/">
+                <a class="button" href="https://www.vankruijsdijknotarissen.nl/" target="blank">
                     Lees meer
                 </a>
             </div>
@@ -81,9 +81,9 @@
                 <img src="{{ asset('storage/uploads/Kunstloc-brabant.png') }}" alt="Logo Kunstloc Brabant">
             </div>
             <div class="text-test">
-                <h2>Kunstloc Brabant</h2>
+                <h3>Kunstloc Brabant</h3>
                 <p>Atty Bax</p>
-                <a class="button" href="https://www.kunstlocbrabant.nl/">
+                <a class="button" href="https://www.kunstlocbrabant.nl/" target="blank">
                     Lees meer
                 </a>
             </div>
@@ -93,9 +93,9 @@
                 <img src="{{ asset('storage/uploads/Nieuwerwets.png') }}" alt="Logo Nieuwerwets">
             </div>
             <div class="text-test">
-                <h2>Nieuwerwets Creatief</h2>
+                <h3>Nieuwerwets Creatief</h3>
                 <p>Roel van Gemert</p>
-                <a class="button" href="https://nieuwerwetscreatief.nl/">
+                <a class="button" href="https://nieuwerwetscreatief.nl/" target="blank">
                     Lees meer
                 </a>
             </div>
@@ -105,9 +105,9 @@
                 <img src="{{ asset('storage/uploads/AVE.jpeg') }}" alt="Logo Albert Verlinde">
             </div>
             <div class="text-test">
-                <h2>Albert Verlinde</h2>
+                <h3>Albert Verlinde</h3>
                 <p>Ik juich de doelstellingen van de stichting "Bureau Onbeperkte Zaken" ten zeerste toe.</p>
-                <a class="button" href="https://nl.wikipedia.org/wiki/Albert_Verlinde">
+                <a class="button" href="https://nl.wikipedia.org/wiki/Albert_Verlinde" target="blank">
                     Lees meer
                 </a>
             </div>
@@ -117,9 +117,9 @@
                 <img src="{{ asset('storage/uploads/Hans-Kroon.jpg') }}" alt="Logo Hans Kroon">
             </div>
             <div class="text-test">
-                <h2>Hans Kroon Advies & Zorg</h2>
+                <h3>Hans Kroon Advies & Zorg</h3>
                 <p>Hans Kroon</p>
-                <a class="button" href="https://hanskroonadvies.nl/">
+                <a class="button" href="https://hanskroonadvies.nl/" target="blank">
                     Lees meer
                 </a>
             </div>
