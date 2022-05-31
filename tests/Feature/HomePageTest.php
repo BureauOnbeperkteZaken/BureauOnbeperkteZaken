60022fba17bcd9c41c28fc3d1b30f7e1785dfbe0
Pixel-Null <rcacouwenberg@gmail.com>
2022-05-31 20:34:18 +0000

    fuked

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Project;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testRecentProjects()
+    {
+        $projects = Project::orderBy('created_at', 'desc')->get()->take(3);
+        $match = '/project/';
+        $page = $this->get('/');
+
+        foreach ($projects as $project) {
+            $this->assertTrue(str_contains($page, $match . $project->id));
+        }
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate:fresh');
+        $this->artisan('db:seed');
+    }
+}
