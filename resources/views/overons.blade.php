cc8f53da4611dd739f2c4a3782b411b649073d65
kjellgeerlings <k.geerlings@student.avans.nl>
2022-05-31 23:41:49 +0200

    test to have similar important information on the website

--- resources/views/overons.blade.php
+++ resources/views/overons.blade.php
@@ -7,6 +7,7 @@ $docs_version = explode('.', Illuminate\Foundation\Application::VERSION)[0] . ".
     <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
             integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <h1 style="text-align: center;">Over Ons</h1>
     </head>
     <x-slot name="header">
         <h2>
