abf63179e1b5599a4fe477586f0e4760adeb1a48
Jente van der Hoek <jr.vanderhoek@student.avans.nl>
2022-06-19 18:51:47 +0200

    Eerste opzet vernieuwde home pagina

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -1,58 +1,62 @@
-<x-page.fullwidth title="Home">
-    <x-slot:background>
-        <iframe id="bigVideo" type="text/html" title="Trailer bureau onbeperkte zaken" src="{{ config('boz.homepage_video_url') }}" allowfullscreen></iframe>
-    </x-slot:background>
-    <div class="home-wrapper">
-        <div class="home-project-gallery">
-            @foreach ($projects as $project)
-            <div class="home-project">
-                <img src="{{ $project->image_path }}" alt="" />
-                <h3>{{ $project->name }}</h3>
-                <p>{{ (strlen($project->description) > 80) ? substr($project->description, 0, 80) . '...' : $project->description; }}</p>
-                <a href="/project/{{ $project->id }}">{{ __('more') }}</a>
+<x-page.fullwidth-novid style="padding-top: 0px;">
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossorigin="anonymous">
+    </head>
+    <div class="home-box">
+        <div class="home-banner" style="background-image: url({{asset('img/BOZ_banner.JPG')}});"></div>
+    </div>
+    <div class="home-setup">
+        <div class="home-text-banner">
+            <h2 class="text-center">Denk liever in mogelijkheden dan beperkingen</h2>
+            <h6 class="text-center" style="color: #525252;">Stichting Bureau Onbeperkte Zaken wil dat mensen met een handicap gezien worden zoals ze zijn, talentvolle mensen. Geen gehandicapten</h6>
+        </div>
+        <div class="home-wrapper">
+            <div>
+                <h1 class="text-center">Wie wij zijn</h1>
+                <div class="home-wzw">
+                    <img class="home-logo" src="{{asset('img/BOZ_logo.JPG')}}" alt="Logo Bureau Onbeperkte Zaken"/>
+                    <p>5 jaar horen we verhalen, wensen en verlangens van mensen met een beperking.
+                        Ons eerste project "Onbeperkt Anders", zal in een aantal korte heftige films
+                        laten zien welke kwaliteiten mensen met een beperking/handicap hebben en hoe
+                        zij in onze samenleving een volwaardige plaats kunnen innemen.</p>
+                </div>
+                <a class="button" href="/overons">Lees meer</a>
             </div>
-            @endforeach
-        </div>
-        <div class="paragraph">
-            <h5>Ons doel</h5>
-            <h2>Een revolutie teweeg brengen! Die inzicht in elkaars werelden geeft</h2>
-            <p>
-                5 jaar horen we verhalen, wensen en verlangens van mensen met een beperking. Ons eerste project "Onbeperkt Anders", zal in een aantal korte heftige films laten zien welke kwaliteiten mensen met een beperking/handicap hebben en hoe zij in onze samenleving een volwaardige plaats kunnen innemen.
-            </p>
-            <ul>
-                <li>Hoe kan ik van alle verhalen in mijn hoofd een boek schrijven zonder te kunnen lezen en schrijven?</li>
-                <li>Kan ik trouwen en samenwonen?</li>
-                <li>Hoe word ik adviseur?</li>
-                <li>Hoe krijg je een volwaardig salaris?</li>
-            </ul>
-        </div>
-        <div class="paragraph">
-            <p>
-                <q>
-                    Ik wil dat mensen met een handicap worden gezien als wie we zijn, talentvolle mensen, dat zijn wij!
-                </q>
-                <span>Tijmen</span>
-            </p>
-        </div>
-        <div class="paragraph">
-            <h2>Wat willen we bereiken</h2>
-            <h3>Onderzoeken</h3>
-            <p>
-                Hoe het VN-verdrag van mensen met een beperking na te leven.
+            <div>
+                <h1 class="text-center">Wat wij doen</h1>
+                <p class="text-center">Bekijk de trailer!</p>
 
-                En bewustwording creëren hoe mensen met een beperking hun talenten kunnen inzetten in de maatschappij.
-            </p>
-            <h3>Acceptatie</h3>
-            <p>
-                <q>
-                    Als je het probleem accepteert is het al voor een deel opgelost.
-                </q>
-                <span>Robin, Theaterwerkplaats Novalis</span>
-            </p>
-            <h3>Co-Creatie</h3>
-            <p>
-                Iedereen moet met elkaar samenwerken. Op de werkvloer, in de kunst en in het dagelijks leven
-            </p>
+                <div class="home-trailer-container">
+                    <iframe src="https://player.vimeo.com/video/715744582?h=f481fd74c0&color=61e0b8&title=0&byline=0&portrait=0"
+                            width="740" height="360"
+                            allow="autoplay; fullscreen; picture-in-picture"
+                            allowfullscreen>
+                    </iframe>
+                </div>
+            </div>
+            <div>
+                <h1 class="text-center">Recente projecten</h1>
+                <div class="home-project-gallery">
+                    @foreach ($projects as $project)
+                        <div class="home-project">
+                            <img src="{{ $project->image_path }}" alt="" />
+                            <h3>{{ $project->name }}</h3>
+                            <p>{{ (strlen($project->description) > 80) ? substr($project->description, 0, 80) . '...' : $project->description; }}</p>
+                            <a href="/project/{{ $project->id }}">{{ __('more') }}</a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <figure>
+                <blockquote class="blockquote">
+                    <p>“Ik wil dat mensen met een handicap worden gezien als wie we zijn, talentvolle mensen, dat zijn wij!”</p>
+                </blockquote>
+                <figcaption class="blockquote-footer">
+                    <cite title="Source Title">Tijmen</cite>
+                </figcaption>
+            </figure>
         </div>
     </div>
-    </x-page>
+</x-page.fullwidth-novid>
